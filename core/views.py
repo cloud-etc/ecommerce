aeff74cc62d02c96b311e573743b7894fd459730
Elvis <dbaelvis@gmail.com>
2019-10-07 21:27:13 -0300

    aula 20 context_processor

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from catalog.models import Category
 
 def index(request):
     return render(request, 'core/index.html')
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'catalog',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # apps
+                'catalog.context_processors.categories',
             ],
         },
     },
